2ec5a6d1e1c147f9951db0fa8c70a7230cc7c4f3
Cheng Huan An <huanan_cheng@mymail.sutd.edu.sg>
2021-05-29 22:53:51 +0800

    mockup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Huan An Cheng';
+  router: string;
+
+  constructor(private _router: Router){
+    this.router = _router.url;
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ExperienceComponent,
+    ContactsComponent,
+    ProjectsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'about', component: AboutComponent},
+      {path: 'experience', component: ExperienceComponent},
+      {path: 'projects', component: ProjectsComponent},
+      {path: 'contact', component: ContactsComponent},
+      {path: '**', component: PageNotFoundComponent}, // this should always be at the end of the array   
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
