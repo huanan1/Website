394ea8a0292e561e91bbde5e94cb214dce903f18
Cheng Huan An <huanan_cheng@mymail.sutd.edu.sg>
2021-06-19 23:18:56 +0800

    added github workflow

--- backend/bootstrap.bat
+++
@@ -1,5 +0,0 @@
-set FLASK_APP=main.py FLASK_DEBUG=true
-
-Powershell.exe -executionpolicy remotesigned -File "c:/Users/shini/OneDrive/Desktop/Personal Website/venv/Scripts/Activate.ps1"
-
-flask run -h 0.0.0.0
